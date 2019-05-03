ccbb657f4c94db85f672b8a269cf503ff1eed2e1
renardete <juanfe_518@hotmail.com>
2019-05-03 17:08:55 -0500

    setup protractor configuration

--- dist/protractor/local.config.js
+++ dist/protractor/local.config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = {
     framework: 'jasmine',
-    specs: ['../test/google.spec.js'],
+    specs: ['../test/google.spec.ts'],
     seleniumAddress: 'http://localhost:4444/wd/hub'
 };
 //# sourceMappingURL=local.config.js.map
