594eca9d0b88ede7364d467fa20d58781f1a4b0b
renardete <juanfe_518@hotmail.com>
2019-05-03 20:42:40 -0500

    add headless capabilities for browser testing

--- dist/protractor/local.config.js
+++ dist/protractor/local.config.js
@@ -5,7 +5,7 @@ const reporter_1 = require("./helpers/reporter");
 exports.config = {
     framework: 'jasmine',
     specs: ['../test/google.spec.js'],
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+    SELENIUM_PROMISE_MANAGER: false,
     onPrepare: () => {
         protractor_1.browser.ignoreSynchronization = true;
         reporter_1.reporter();
